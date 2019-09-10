e3ff016565eabd17fd694deebc514de1fb781d6b
Kathy B <kathybuggs@gmail.com>
2019-09-10 17:51:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,11 +3,10 @@ require 'pry'
 
 def load_library(file_path)
   emoticon = YAML.load_file("#{file_path}")
+  
   emo = {get_meaning: {}, get_emoticon: {} }
-      #emo = {}
-      #emo[:get_emoticon] = {#}
+  
     emoticon.each do |meaning, value|
- 
       emo[:get_meaning][meaning] = value[1] 
       emo[:get_emoticon][meaning] = value[0]
       #emo[0][0] = {japanese_emoticon: value[1], english_emoticon: value[0]}
