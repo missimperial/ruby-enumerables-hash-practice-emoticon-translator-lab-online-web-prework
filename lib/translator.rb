bb8e2291723ca0d967fe85152ed72bf319ce9f12
Kathy B <kathybuggs@gmail.com>
2019-09-10 18:16:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,10 +14,11 @@ def load_library(file_path)
     emo
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
+  emo_hash = load_library(file_path)
   
   
-  # binding.pry
+   binding.pry
 end
 
 def get_english_meaning
