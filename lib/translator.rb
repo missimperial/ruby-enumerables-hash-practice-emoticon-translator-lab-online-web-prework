1157645ee9a9eb53431096a077e7ee18ba5deee1
Kathy B <kathybuggs@gmail.com>
2019-09-10 18:01:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file_path)
   
     emoticon.each do |meaning, value|
       emo[:get_meaning][value[1]] = meaning
-      emo[:get_emoticon][value[0]] = meaning
+      emo[:get_emoticon][value[0]] = emoticon[meaning]
       #emo[0][0] = {japanese_emoticon: value[1], english_emoticon: value[0]}
       
       #binding.pry
